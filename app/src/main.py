1b0a8966a61248d2e3d20f1fead67503d0587675
igorrr <id.nikto@gmail.com>
2025-05-27 08:32:45 +0300

    remove commented

--- app/src/main.py
+++ app/src/main.py
@@ -2,9 +2,11 @@ import os
 from fastapi import FastAPI, Request, Response
 import httpx
 
-app = FastAPI()
 API_KEY = os.getenv("API_KEY")
 API_HOST = os.getenv("API_HOST")
+
+app = FastAPI()
+
 
 @app.api_route(
     "/api-v1/{path:path}",
@@ -26,10 +28,6 @@ async def proxy(path: str, request: Request):
     return Response(
         content=resp.content,
         status_code=resp.status_code,
-        # headers={
-        #     k: v for k, v in resp.headers.items()
-        #     if k.lower() not in ("transfer-encoding", "connection")
-        # },
         headers=resp.headers,
         media_type=resp.headers.get("content-type"),
     )
